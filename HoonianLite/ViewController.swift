57fc00085dd4132e6f5f10a5169e94c4e2eefd3c
santmalvin17 <malvinsantoso023@gmail.com>
2020-07-18 15:24:21 +0700

    commit

---
+++ HoonianLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoonianLite
+//
+//  Created by Malvin Santoso on 18/07/20.
+//  Copyright © 2020 Malvin Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

db62c662c2dddc552870ce1e47d1ab4ef197f403
santmalvin17 <malvinsantoso023@gmail.com>
2020-07-21 23:48:50 +0700

    fix error

--- HoonianLite/Class/Home/HomeViewController.swift
+++ HoonianLite/Class/Home/HomeViewController.swift
@@ -230,6 +230,7 @@ extension HomeViewController: HomeUpcomingProjectContentTableViewCellDelegate {
 
 extension HomeViewController: HomeLocationContentTableViewCellDelegate, HomeApartmentContentTableViewCellDelegate, HomeLandedPropertyContentTableViewCellDelegate, HomeWarehouseContentTableViewCellDelegate {
     func homeLocationSelected(indexKe: Int) {
+
         ACRequest.GET_PROJECT_LIST(limitPage: "5", page: "1", selectCityId: ACData.LOGINDATA.homeData.cities[indexKe].id, selectCategoryId: ACData.LOGINDATA.homeData.apartment.categoryId,  successCompletion: { (projectList) in
             ACData.PROJECTLISTMODEL = projectList
             SVProgressHUD.dismiss()
@@ -246,6 +247,14 @@ extension HomeViewController: HomeLocationContentTableViewCellDelegate, HomeApar
     
     func homeApartmentSelected(indexKe:Int) {
         
+        ACRequest.GET_PROJECT_LIST(limitPage: "5", page: "1", selectCityId: ACData.LOGINDATA.homeData.cities[indexKe].id, selectCategoryId: ACData.LOGINDATA.homeData.apartment.categoryId,  successCompletion: { (projectList) in
+            ACData.PROJECTLISTMODEL = projectList
+            SVProgressHUD.dismiss()
+        }) { (message) in
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
         ACRequest.GET_PROJECT_DETAIL(id: ACData.LOGINDATA.homeData.apartment.projects[indexKe].id,  successCompletion: { (projectDetail) in
                 ACData.PROJECTDETAILMODEL = projectDetail
                 SVProgressHUD.dismiss()
